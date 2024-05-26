f8fa59799a9c36e58f61547df40bcfc7de2f27ec
Vlad <vkrytii@gmail.com>
2024-05-26 20:20:57 +0300

    add logic to add dish

--- CaloriesApplication/Pages/AddDishPage.xaml.cs
+++ CaloriesApplication/Pages/AddDishPage.xaml.cs
@@ -1,10 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using CaloriesApplication.Data;
 
 namespace CaloriesApplication
 {
-    /// <summary>
-    /// Interaction logic for AddDishPage.xaml
-    /// </summary>
     public partial class AddDishPage : Page
     {
         public AddDishPage()
@@ -12,6 +11,28 @@ namespace CaloriesApplication
             InitializeComponent();
         }
 
+        private void AddDishButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            string kiloCalories = KiloCaloriesTextBox.Text;
+            string description = DescriptionTextBox.Text;
 
+            EnteredDataTextBox.Text = $"Dish Name :: {name}\nKiloCaloriesPer100Grams :: {kiloCalories}\nDescription :: {description}";
+
+            using (var context = new FoodDbContext())
+            {
+                var dish = new Dish
+                {
+                    Name = name,
+                    KiloCaloriesPer100Grams = decimal.Parse(kiloCalories),
+                    Description = description
+                };
+
+                context.Dishes.Add(dish);
+                context.SaveChanges();
+            }
+
+            MessageBox.Show("Added!", "Confirm", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
