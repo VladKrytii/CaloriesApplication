724402996be8992f83a872eab282b4478f83b484
oV1ce e <omax51851@gmail.com>
2024-05-26 11:00:59 +0300

    im the greate

--- CaloriesApplication/Pages/NeededCaloriesPage.xaml.cs
+++ CaloriesApplication/Pages/NeededCaloriesPage.xaml.cs
@@ -28,11 +28,36 @@ namespace CaloriesApplication
                 double weight = double.Parse(weightTextBox.Text);
                 double age = double.Parse(ageTextBox.Text);
                 string gender = genderCombo.SelectedItem.ToString();
+                string trueGender = "";
+                if (gender.Contains("Male"))
+                {
+                    trueGender = "Male";
+                }
+                else if (gender.Contains("Female"))
+                {
+                    trueGender = "Female";
+                }
                 string exercise = loadCombo.SelectedItem.ToString();
-
+                string trueExercise = "";
+                if (exercise.Contains("Light exercise (1–3 days per week)"))
+                {
+                    trueExercise = "Light exercise (1–3 days per week)";
+                }
+                else if (exercise.Contains("Moderate exercise (3–5 days per week)"))
+                {
+                    trueExercise = "Moderate exercise (3–5 days per week)";
+                }
+                else if (exercise.Contains("Heavy exercise (6–7 days per week)"))
+                {
+                    trueExercise = "Heavy exercise (6–7 days per week)";
+                }
+                else if (exercise.Contains("Very heavy exercise (twice per day, extra heavy workouts)"))
+                {
+                    trueExercise = "Very heavy exercise (twice per day, extra heavy workouts)";
+                }
                 // Calculate calories
                 CalculationLogic calcLogic = new CalculationLogic();
-                double calories = calcLogic.CalculateCalories(height, weight, age, gender, exercise);
+                double calories = calcLogic.CalculateCalories(height, weight, age, trueGender, trueExercise);
 
                 // Display result
                 bmrDisplay.Text = $"Your base metabolic rate burns {calories} calories";
