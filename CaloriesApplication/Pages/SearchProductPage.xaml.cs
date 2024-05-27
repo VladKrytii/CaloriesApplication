1a09702c2b608d4dd9602d96e5de0b36c75bae18
Vlad <vkrytii@gmail.com>
2024-05-27 21:03:55 +0300

    add logit to search product add change size of MainWindow

--- CaloriesApplication/Pages/SearchProductPage.xaml.cs
+++ CaloriesApplication/Pages/SearchProductPage.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using CaloriesApplication.Data;
 
 namespace CaloriesApplication
 {
-    /// <summary>
-    /// Interaction logic for SearchProductPage.xaml
-    /// </summary>
     public partial class SearchProductPage : Page
     {
         public SearchProductPage()
@@ -12,12 +13,44 @@ namespace CaloriesApplication
             InitializeComponent();
         }
 
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = ProductNameTextBox.Text;
+            string kiloCalories = KiloCaloriesTextBox.Text;
 
+            using (var context = new FoodDbContext())
+            {
+                var query = context.Products.AsQueryable();
 
+                if (!string.IsNullOrWhiteSpace(productName))
+                {
+                    query = query.Where(p => p.Name.Contains(productName));
+                }
 
+                if (!string.IsNullOrWhiteSpace(kiloCalories))
+                {
+                    if (decimal.TryParse(kiloCalories, out decimal calories))
+                    {
+                        query = query.Where(p => p.KiloCaloriesPer100Grams == calories);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid number for kilo calories", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                }
 
+                List<Product> searchResults = query.ToList();
+                SearchResultsListView.ItemsSource = searchResults;
+            }
+        }
 
+        private void ProductNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
 
-
+        private void KiloCaloriesTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
     }
 }
