d432b8710734c5aa90dcfb83bd0669774917144a
Vlad <vkrytii@gmail.com>
2024-05-27 18:44:32 +0300

    add changes to namespace, to max branch

--- CaloriesApplication/Database/FoodDbContext.cs
+++ CaloriesApplication/Database/FoodDbContext.cs
@@ -10,7 +10,7 @@ namespace CaloriesApplication.Database
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
-            {
+            {   
                 optionsBuilder.UseSqlite("Data Source=Food.db");
             }
         }
