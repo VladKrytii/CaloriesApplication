fe6d986280eded80df55fad34c7ad65590543e58
Vlad <vkrytii@gmail.com>
2024-05-27 20:49:25 +0300

    add logic to search product

--- CaloriesApplication/Pages/SearchDishPage.xaml.cs
+++ CaloriesApplication/Pages/SearchDishPage.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using CaloriesApplication.Data;
 
 namespace CaloriesApplication
 {
-    /// <summary>
-    /// Interaction logic for SearchDishPage.xaml
-    /// </summary>
     public partial class SearchDishPage : Page
     {
         public SearchDishPage()
@@ -12,8 +13,46 @@ namespace CaloriesApplication
             InitializeComponent();
         }
 
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string dishName = DishNameTextBox.Text;
+            string kiloCalories = KiloCaloriesTextBox.Text;
 
+            using (var context = new FoodDbContext())
+            {
+                var query = context.Dishes.AsQueryable();
 
+                if (!string.IsNullOrWhiteSpace(dishName))
+                {
+                    query = query.Where(d => d.Name.Contains(dishName));
+                }
 
+                if (!string.IsNullOrWhiteSpace(kiloCalories))
+                {
+                    if (decimal.TryParse(kiloCalories, out decimal calories))
+                    {
+                        query = query.Where(d => d.KiloCaloriesPer100Grams == calories);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid number for kilo calories", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    } 
+                }
+
+                List<Dish> searchResults = query.ToList();
+                SearchResultsListView.ItemsSource = searchResults;
+            }
+        }
+
+        private void DishNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void KiloCaloriesTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
